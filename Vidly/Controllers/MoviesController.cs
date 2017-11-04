2fef7b8199750c583fbb72cadc3ede1fd0c47233
Matt Durrant <Matt.Durrant@northofenglandcpc.onmicrosoft.com>
2017-11-04 14:05:37 +0000

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies route
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Die Hard" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("You entered id " + id);
+        }
+    }
+}
