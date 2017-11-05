ef7e199778ab75ebd7ebb9e71d317b9520c63a17
Matt Durrant <sum0199@gmail.com>
2017-11-05 23:20:40 +0000

    end of part 3

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ViewResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+
+        //[Route("Customer/Detail/{id}")]
+        public ActionResult Detail(int id)
+        {
+            Customer customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null) return HttpNotFound();
+            return View(customer);
+        }
+
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,24 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre);
+            return View(movies);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            Movie movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null) return HttpNotFound();
+            return View(movie);
+        }
+
+
         // GET: Movies route
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Die Hard" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name="Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id)
         {
             return Content("You entered id " + id);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
+
     }
 }
